7fd019bf61c6df80ab732e4af8d61f1e31770005
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-06-16 21:13:29 +0300

    Add checkboxes and do some refactoring

--- public/js/todoItem.js
+++ public/js/todoItem.js
@@ -1,6 +1,12 @@
 var TodoItem = Backbone.Model.extend({
+	defaults: {
+		isCompleted: false
+	},
 	validate: function(attrs) {
 		if(!attrs.title)
 			return "title is required";
+	},
+	toggle: function(){
+		this.set("isCompleted", !this.get("isCompleted"));
 	}
 });
--- public/js/todoItemView.js
+++ public/js/todoItemView.js
@@ -3,9 +3,23 @@ var TodoItemView = Backbone.View.extend({
 	initialize: function(options){
 		if (!(options && options.model))
 			throw new Error("Model is not specified");
+		this.model.on("change", this.render, this);
 	},
+
+	events: {
+		"click .toggle": "onClickToggle"
+	},
+
+	onClickToggle: function(){
+		this.model.toggle();
+		console.log(this.model.toJSON());
+	},
+
 	render: function(){
-		this.$el.html(this.model.escape("title"));
+		this.$el.toggleClass('completed', this.model.get("isCompleted"));
+
+		var checked = this.model.get("isCompleted") ? "checked" : "";
+		this.$el.html("<input class='toggle' type='checkbox'"+checked+"></input> "+this.model.escape("title"));
 		return this;
 	}
 });
